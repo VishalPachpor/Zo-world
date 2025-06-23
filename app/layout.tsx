cf747d6e136bb7ba7997b3151bf51e98df99a9a4
VishalPachpor <vishal@stratifi.xyz>
2025-06-23 23:05:42 +0530

    Favicon added

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,14 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "EAK Accelerator",
-  description: "EAK Accelerator",
+  description:
+    "EAK Accelerator - Fueling the next wave of innovation at the intersection of AI and blockchain",
   generator: "EAK Accelerator",
+  icons: {
+    icon: [{ url: "/logo.png" }, { url: "/favicon.png" }],
+    shortcut: "/favicon.png",
+    apple: "/logo.png",
+  },
 };
 
 export default function RootLayout({
@@ -19,6 +25,8 @@ export default function RootLayout({
           href="https://fonts.cdnfonts.com/css/tt-hoves-pro-trial"
           rel="stylesheet"
         />
+        <link rel="icon" type="image/png" href="/favicon.png" />
+        <link rel="apple-touch-icon" href="/logo.png" />
       </head>
       <body className="font-sans">{children}</body>
     </html>
